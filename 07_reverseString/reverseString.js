5c8f8e662d3f00326495e07b2257f6c9ee8a4d99
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-03 21:30:41 +0200

    Add loop and temp variable to make reverseString functionable and pass test 1

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,12 +1,45 @@
-const reverseString = function(string) {
+const reverseString = function(string) { //receive string as argument
 
-    string = string.split('');
+    string = string.split(''); //split string into array
 
-    return string;
+    let temp = "";//create empty variable to store swapped letter
+
+    let lastIndex = string.length - 1; //store last index in variable for easier write
+
+
+   for (let i = 0; i < string.length; i++) { //create loop to run through array by limit length
+
+        temp = string[i]; //store initial swapped letter
+
+        string[i] = string[lastIndex];    //replace initial letter with the last
+
+        string[lastIndex] = temp; //store temp initial letter in last index
+
+        lastIndex--; //decrement index
+
+        if (lastIndex === 1) {
+            /*
+
+            I have no idea why this works, but i console logged the string array
+            to monitor letter swapping or replacement process (also the i, temp and lastIndex), 
+            and i noticed when last index was equal to one, 
+            the reverse letters were perfectly done
+            so when it conitnued to be decremneted to 0, the array broke and the 
+            array returned to its original state, so i break the loop when 
+            lastindex is equal to 1, and it works, thank youuuu buauauaaauau!
+
+            */
+
+            break; 
+        }
+        
+   }
+
+    return string.join(""); //return joined string
 
 };
 
-console.log(reverseString('hello'));
+//console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
