393f1616733c66377277723750a00d46ebb9c203
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-03 11:43:41 +0200

    Implement repeatString to pass the first 5 tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+
+        //new empty string
+        let word = "";
+
+
+        for (let i = 0; i < number; i++) {
+
+            //concatenate word by string by x times
+            word =  word + string;
+
+        }
+
+        //return concatenated string
+
+        return word;
 
 };
+
+//console.log(repeatString("hi",3));
 
 // Do not edit below this line
 module.exports = repeatString;
--- 06_repeatString/repeatString.spec.js
+++ 06_repeatString/repeatString.spec.js
@@ -4,16 +4,16 @@ describe('repeatString', () => {
   test('repeats the string', () => {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  test.skip('repeats the string many times', () => {
+  test('repeats the string many times', () => {
     expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hi', 1)).toEqual('hi');
   });
-  test.skip('repeats the string 0 times', () => {
+  test('repeats the string 0 times', () => {
     expect(repeatString('bye', 0)).toEqual('');
   });
-  test.skip('does not use the built-in String repeat method', () => {
+  test('does not use the built-in String repeat method', () => {
     /* Even though there is a built-in String repeat method,
       in this exercise specifically, we want you to practise using loops */
     jest.spyOn(String.prototype, 'repeat').mockName('Built-in String repeat method');
