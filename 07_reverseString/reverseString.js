f15c9887c281490adadfe0555ce669029b0554a5
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-03 14:55:38 +0200

    Add argument and string return and fail first test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    return string;
 
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
