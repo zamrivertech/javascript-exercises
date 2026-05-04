d879e7fbe532acbfdc3f7ba5405361baa49b1042
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-04 13:42:13 +0200

    Save same value implementation progress

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -4,6 +4,8 @@ const removeFromArray = function(array, ...params) {
 
     paramLength.push(...params);
 
+    /*
+
     if (paramLength.length === 1) {
      
         array.splice(array.indexOf(params[0]),1);
@@ -12,16 +14,27 @@ const removeFromArray = function(array, ...params) {
         
     }
 
+    */
+
     for (let i = 0; i < array.length; i++) {
 
-        array.splice(array.indexOf(params[i]),1);
+
+        console.log("params[i]: " + params[0] + " array[i]: " + array[i]);
+        console.log(params[0] === array[i]);
+
+
+        if (params[0] === array[i]) {
+
+
+            array.splice(array.indexOf(params[0]),1);
+        }
 
     }
 
     return array;
 };
 
-console.log(removeFromArray(["A","B","C"], "B", "A"));
+console.log(removeFromArray(["A","B","B","C"], "B"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
