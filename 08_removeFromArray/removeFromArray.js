706b38b64e169cfaf42b914efa373f385a5a3769
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-04 21:46:33 +0200

    Restart from exercise and fail remove single value test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,40 +1,9 @@
-const removeFromArray = function(array, ...params) {
+const removeFromArray = function() {
 
-    let paramLength = [];
-
-    paramLength.push(...params);
-
-    /*
-
-    if (paramLength.length === 1) {
-     
-        array.splice(array.indexOf(params[0]),1);
-
-        return array;
-        
-    }
-
-    */
-
-    for (let i = 0; i < array.length; i++) {
-
-
-        console.log("params[i]: " + params[0] + " array[i]: " + array[i]);
-        console.log(params[0] === array[i]);
-
-
-        if (params[0] === array[i]) {
-
-
-            array.splice(array.indexOf(params[0]),1);
-        }
-
-    }
-
-    return array;
+    
 };
 
-console.log(removeFromArray(["A","B","B","C"], "B"));
+//console.log(removeFromArray(["A","B","B","C"], "B"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 08_removeFromArray/removeFromArray.spec.js
+++ 08_removeFromArray/removeFromArray.spec.js
@@ -4,10 +4,10 @@ describe('removeFromArray', () => {
   test('removes a single value', () => {
     expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-  test('removes multiple values', () => {
+  test.skip('removes multiple values', () => {
     expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
   });
-  test('removes multiple of the same value', () => {
+  test.skip('removes multiple of the same value', () => {
     expect(removeFromArray([1, 2, 2, 3], 2)).toEqual([1, 3]);
   });
   test.skip('ignores non present values', () => {
