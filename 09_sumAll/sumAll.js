f04ffc3cca517c17f3a8a24c551a51a69aed75f1
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-05 23:51:28 +0200

    Save initial progress in SumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    let counter = a;
+
+    let sum = [];
+
+
+
+    while (counter <= b) {
+
+        sum.push(counter);
+
+        counter++;
+    }
+
+    return sum.reduce(a);
 
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
