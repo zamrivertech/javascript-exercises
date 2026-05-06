efc54551a88951263f0b03e2d40c923fa1309a2b
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-06 21:42:55 +0200

    Implement sumAll function to pass sum numbers within range test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,23 +1,31 @@
 const sumAll = function(a,b) {
 
-    let counter = a;
+    let counter = a; //create counter to a
 
-    let sum = [];
+    let sumArray = []; //create empty sum array
 
+    let sum = 0;
 
+    while (counter <= b) { //while counter is less or equal to b
 
-    while (counter <= b) {
+        sumArray.push(counter); //add the initial value and following increment value till loop... 
+                            //...condition is false to break the loop
 
-        sum.push(counter);
+        counter++; //and increment the initial value to 1 till its equal to b
 
-        counter++;
     }
 
-    return sum.reduce(a);
+    for (let i = 0; i < sumArray.length; i++) {//create loop to run through array
+
+        sum = sum + sumArray[i]; //sum with array elements to get final sum
+
+    }
+
+    return sum;//return final sum
 
 };
 
-console.log(sumAll(1,4));
+console.log(sumAll(2,4));
 
 // Do not edit below this line
 module.exports = sumAll;
