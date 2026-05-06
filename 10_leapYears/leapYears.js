cbe5fe7b759ada3b6f23125d95359202162f6f74
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-06 23:27:50 +0200

    Pass non century years with 1900 test

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,7 @@
 const leapYears = function(year) { //add year param
 
-    if (year % 4 === 0) {// if divisible by 4 then return true
+    if (year % 4 === 0 && !(year % 100 === 0)) { // if divisible by 4 and not divisible by 100
+                                                //  then return true
         return true;
     } 
 
--- 10_leapYears/leapYears.spec.js
+++ 10_leapYears/leapYears.spec.js
@@ -10,7 +10,7 @@ describe('leapYears', () => {
   test('works with ridiculously futuristic non century years', () => {
     expect(leapYears(34992)).toBe(true);
   });
-  test.skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1900)).toBe(false);
   });
   test.skip('works with century years', () => {
