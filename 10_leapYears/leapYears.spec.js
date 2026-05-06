8ead0150ee027e6fad7e72ab27a0e72e2d66f828
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-06 23:28:24 +0200

    Fail non century years with 1600 test

--- 10_leapYears/leapYears.spec.js
+++ 10_leapYears/leapYears.spec.js
@@ -13,7 +13,7 @@ describe('leapYears', () => {
   test('works with century years', () => {
     expect(leapYears(1900)).toBe(false);
   });
-  test.skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1600)).toBe(true);
   });
   test.skip('works with century years', () => {
