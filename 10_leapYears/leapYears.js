cac1c4f32e02386a1056bb0beea7c223034bba8b
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-06 23:39:21 +0200

    Pass century year -1600-  test

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) { //add year param
 
-    if (year % 4 === 0 && !(year % 100 === 0)) { // if divisible by 4 and not divisible by 100
-                                                //  then return true
+    if ( (year % 4 === 0 && !(year % 100 === 0)) || (year % 400 === 0)) { 
+        //if divsible by 4 and not by 100, is leap year, but if divisible by 400, is leap year
         return true;
     } 
 
